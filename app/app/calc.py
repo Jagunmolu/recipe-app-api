f1bb277c2b2d04fd0300513d2b594530c719b0a9
Jagunmolu <kolawolejonathan@gmail.com>
2023-02-12 20:35:28 +0100

    wait_for_db setup.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,5 +1,6 @@
 def multiply(a: int, b: int):
     return a * b
 
+
 def difference(a: int, b: int) -> int:
     return abs(a - b)
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,16 +1,13 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
     def test_multiply_numbers(self):
-
         res = calc.multiply(2, 51)
-
         self.assertEqual(res, 102)
 
     def test_difference(self):
-
         res = calc.difference(24, 41)
-
         self.assertEqual(res, 17)
